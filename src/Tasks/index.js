602ea789efdda06ed583401de1a3d1f4223d3cbe
Patryk Majchrzak <pmaj12345@gmail.com>
2024-07-02 23:45:51 +0200

    restructuring props in Tasks component

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,9 +1,9 @@
 import "./style.css"
 
-const Tasks = (props) => (
+const Tasks = ({tasks, hideDone}) => (
     <ul>
-        {props.tasks.map(task => (
-            <li className={`list__item ${task.done && props.hideDone ? "list__item--hidden" : ""}`}>
+        {tasks.map(task => (
+            <li className={`list__item ${task.done && hideDone ? "list__item--hidden" : ""}`}>
                 <button className="list__button list__button--toggleDone" />
                     <span className={task.done ? "list__item--done" : ""}>
                         {task.content}
