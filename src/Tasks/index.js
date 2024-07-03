48e2abee6d9ef0d6384630d6f05ba1235fe8f0f1
Patryk Majchrzak <pmaj12345@gmail.com>
2024-07-03 18:03:59 +0200

    adding ✔ and 🗑 to list buttons

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -4,11 +4,15 @@ const Tasks = ({tasks, hideDone}) => (
     <ul>
         {tasks.map(task => (
             <li className={`list__item ${task.done && hideDone ? "list__item--hidden" : ""}`} key={task.id}>
-                <button className="list__button list__button--toggleDone" />
+                <button className="list__button list__button--toggleDone">
+                    {task.done ? "✔" : ""} 
+                </button>
                     <span className={task.done ? "list__item--done" : ""}>
                         {task.content}
                     </span>
-                <button className="list__button list__button--remove" />
+                <button className="list__button list__button--remove">
+                    🗑
+                </button>
             </li>
         ))}
     </ul>
