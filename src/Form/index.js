b318b0aa6e357b3db680adaaeffee2bcca3d93db
Patryk Majchrzak <pmaj12345@gmail.com>
2024-07-11 12:48:32 +0200

    setting focus on input after submitting a form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,22 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { FormContainer, Input, Button } from "./styled";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
-
+    const inputRef = useRef(null);
+    
     const onFormSubmit = (event) => {
         event.preventDefault();
         
         const newTaskTrimmed = newTaskContent.trim();
-        const resetTaskContent = () => setNewTaskContent(newTaskContent => newTaskContent="")
+        const resetTaskContent = () => setNewTaskContent(newTaskContent => newTaskContent="");
 
         if(!newTaskTrimmed) {
             resetTaskContent();
+            inputRef.current.focus()
             return;
         }
 
         addNewTask(newTaskTrimmed);
         resetTaskContent();
+        inputRef.current.focus()
     };
 
     return (
@@ -25,6 +28,7 @@ const Form = ({ addNewTask }) => {
                 placeholder="Co jest do zrobienia?"
                 value={newTaskContent}
                 onChange={({ target }) => setNewTaskContent(target.value)}
+                ref={inputRef}
             />
             <Button>Dodaj zadanie</Button>
         </FormContainer>
