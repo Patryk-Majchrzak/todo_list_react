5bab08f6ac2571fce28104d37694b4e5da20cf89
Patryk Majchrzak <pmaj12345@gmail.com>
2024-07-11 19:06:41 +0200

    rnaming Header to MainHeader and centralize it

--- src/Header/index.js
+++
@@ -1,5 +0,0 @@
-const MainHeader = ({title}) => (
-    <h1>{title}</h1>
-);
-
-export default MainHeader;
---
+++ src/MainHeader/index.js
@@ -0,0 +1,7 @@
+import { Header } from "./styled";
+
+const MainHeader = ({title}) => (
+    <Header>{title}</Header>
+);
+
+export default MainHeader;
