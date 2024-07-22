c67814e9c2f7e5c3ac7681169041ab6f778534d2
Patryk Majchrzak <pmaj12345@gmail.com>
2024-07-22 18:33:01 +0200

    white spaces added to GlobalStyle

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,15 +2,15 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     html{
-    box-sizing: border-box;
+        box-sizing: border-box;
     }
 
     *, ::before, ::after{
-    box-sizing: inherit;
+        box-sizing: inherit;
     }
 
     body{
-    background-color: ${({theme}) => theme.colors.background};
-    font-family: 'Montserrat', sans-serif;
+        background-color: ${({theme}) => theme.colors.background};
+        font-family: 'Montserrat', sans-serif;
     }
 `
