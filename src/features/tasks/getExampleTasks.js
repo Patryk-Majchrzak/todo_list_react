ad6b9e2d11001c4091d7b2392f17e5c1bd3c1245
Patryk Majchrzak <pmaj12345@gmail.com>
2024-07-23 21:36:49 +0200

    changing fetch link

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -4,7 +4,7 @@ import i18next from "i18next";
 export const getExampleTasks = async () => {
     try {
         const language = i18next.language
-        const response = await axios.get(`/locales/${language}/exampleTasks.json`)
+        const response = await axios.get(`/todo_list_react/locales/${language}/exampleTasks.json`)
         return response
     } catch (error) {
         alert("Unexpected error occured, please try again later")
