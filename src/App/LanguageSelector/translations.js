0689d97e5e6ada6d10572ece5303bcb8c55c2178
Patryk Majchrzak <pmaj12345@gmail.com>
2024-07-23 11:31:02 +0200

    created translations file

---
+++ src/App/LanguageSelector/translations.js
@@ -0,0 +1,98 @@
+const translations = {
+    Navigation: {
+        pl: {
+            section_1: "Zadania",
+            section_2: "O autorze",
+            language: "Wybierz język"
+        },
+        en: {
+            section_1: "Tasks",
+            section_2: "About author",
+            language: "Choose language"
+        },
+    },
+    Author: {
+        pl: {
+            title: "O autorze",
+            greeting: "Cześć! 🖐️",
+            name: "Nazywam się",
+            age: "i mam 29 lat.",
+            workPlace: "Przez około 6 lat pracowałem w Infosysie",
+            workPosition: "jako księgowy dla zagranicznych klientów (głównie dla filii angielskich, irlandzkich oraz kanadyjskich).",
+            future: "Postanowiłem spróbować swoich sił jako front-end developer, a w przyszłości być może full-stack",
+        },
+        en: {
+            title: "About the author",
+            greeting: "Hello! 🖐️",
+            name: "My name is",
+            age: "and I'm 29 years old.",
+            workPlace: "I worked for about 6 years at Infosys",
+            workPosition: "as an accountant for foreign clients (mainly for English, Irish, and Canadian branches).",
+            future: "I decided to try my hand as a front-end developer, and maybe full-stack in the future",
+        }
+    },
+    exampleTasks: {
+        pl: {
+            task_1: "zrobić coś",
+            task_2: "zrobić coś inneego",
+        },
+        en: {
+            task_1: "do something",
+            task_2: "do something else",
+        },
+    },
+    singleTaskView: {
+        pl: {
+            done: "Ukończono",
+            confirmation: "tak",
+            negation: "nie",
+        },
+        en: {
+            done: "Done",
+            confirmation: "yes",
+            negation: "no"
+        },
+    },
+    header: {
+        pl: "Lista zadań",
+        en: "Tasks list",
+    },
+    addTaskSection: {
+        pl: {
+            header: "Dodaj nowe zadanie",
+            placeholder: "Co jest do zrobienia",
+            addTaskButton: "Dodaj zadanie",
+            downloadExampleTasksButton: "Pobierz przykładowe zadania",
+        },
+        en: {
+            header: "Add new task",
+            placeholder: "What needs to be done?",
+            addTaskButton: "Add task",
+            downloadExampleTasksButton: "Download example tasks",
+        },
+    },   
+    searchSection: {
+        pl: {
+            header: "Wyszukiwarka",
+            placeholder: "Szukaj zadania",
+        },
+        en: {
+            header: "Search engine",
+            placeholder: "Search for tasks",
+        },
+    },
+    tasksSection: {
+        pl: {
+            header: "Lista zadań",
+            markAllDoneButton: "Ukończ wszystkie",
+            showDone: "Pokaż ukończone",
+            hideDone: "Ukryj ukończone",
+        },
+        en: {
+            header: "Tasks list",
+            markAllDoneButton: "Finish all tasks",
+            showDone: "Show finished tasks",
+            hideDone: "Hide finished tasks",
+        },
+    },
+  };
